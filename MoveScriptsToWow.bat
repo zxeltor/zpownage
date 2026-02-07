6b354842d689aea2d75fede3d88f01a2d83fa8fd
Zxeltor <todd.taylor.ee@gmail.com>
2026-02-07 14:36:47 -0500

    Moving files around

---
+++ MoveScriptsToWow.bat
@@ -0,0 +1,4 @@
+
+copy *.lua "C:\Program Files (x86)\World of Warcraft\_retail_\Interface\AddOns\ZPownage\"
+
+copy *.toc "C:\Program Files (x86)\World of Warcraft\_retail_\Interface\AddOns\ZPownage\"
