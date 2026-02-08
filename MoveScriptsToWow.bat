d8f41e5a95eefdcc5b8c73a2af7de19c3393b599
Todd Taylor <todd.taylor.ee@gmail.com>
2026-02-08 15:58:07 -0500

    Delete MoveScriptsToWow.bat

--- MoveScriptsToWow.bat
+++
@@ -1,4 +0,0 @@
-
-copy *.lua "C:\Program Files (x86)\World of Warcraft\_retail_\Interface\AddOns\ZPownage\"
-
-copy *.toc "C:\Program Files (x86)\World of Warcraft\_retail_\Interface\AddOns\ZPownage\"
